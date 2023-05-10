2951d14912dd4ac2be7edf975b7e707bb7eed9ef
matdomichard <mathieu.domichard@gmail.com>
2023-05-10 23:23:22 +0200

    Ajout de la carte des départements

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,35 @@
 <template>
   <img alt="Vue logo" src="./assets/logo.png" />
   <TestApiComponent />
+  <MapComponent @departement-select="handleDepartementSelect" />
+  <div v-if="selectedDepartement">
+    Département sélectionné : {{ selectedDepartement }}
+  </div>
 </template>
 
 <script lang="ts">
-import { defineComponent } from "vue";
+import { defineComponent, ref } from "vue";
 import TestApiComponent from "./components/TestApiComponent.vue";
+import MapComponent  from "./components/MapComponent.vue" 
 
 export default defineComponent({
   name: "App",
   components: {
     TestApiComponent,
+    MapComponent
+  },
+  setup() {
+    const selectedDepartement = ref(""); // Variable pour stocker le département sélectionné
+
+    // Fonction pour gérer la sélection du département
+    const handleDepartementSelect = (departementCode: string) => {
+      selectedDepartement.value = departementCode; // Stockage de la valeur émise dans la variable
+    };
+
+    return {
+      selectedDepartement,
+      handleDepartementSelect,
+    };
   },
 });
 </script>
--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,10 @@
 import { createApp } from "vue";
 import App from "./App.vue";
+import { LMap, LTileLayer,LGeoJson } from "@vue-leaflet/vue-leaflet";
+import "leaflet/dist/leaflet.css";
 
-createApp(App).mount("#app");
+const app = createApp(App);
+app.component("l-map", LMap);
+app.component("l-tile-layer", LTileLayer);
+app.component("l-geo-json", LGeoJson);
+app.mount("#app");
