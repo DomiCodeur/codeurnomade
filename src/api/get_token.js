21d4789f1083c6e76d46c629978d61e7bdadcc13
matdomichard <mathieu.domichard@gmail.com>
2023-05-16 13:37:48 +0200

    Modification pour l'environnement de production

--- src/api/get_token.js
+++ src/api/get_token.js
@@ -1,12 +1,7 @@
 require("dotenv").config();
-const express = require("express");
-const cors = require("cors");
 const axios = require("axios");
-const app = express();
 
-app.use(cors());
-
-app.post("/get_token", async (req, res) => {
+module.exports = async (req, res) => {
   const tokenUrl =
     "https://entreprise.pole-emploi.fr/connexion/oauth2/access_token?realm=/partenaire";
   const clientId = process.env.POLE_EMPLOI_CLIENT_ID;
@@ -26,14 +21,10 @@ app.post("/get_token", async (req, res) => {
         scope: scope,
       },
     });
+
     res.json(response.data);
   } catch (error) {
     console.error(error);
     res.status(500).json({ message: "Error fetching token" });
   }
-});
-
-const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`);
-});
+};
--- vue.config.js
+++ vue.config.js
@@ -1,9 +1,8 @@
-
 module.exports = {
   devServer: {
     proxy: {
       '/get_token': {
-        target: 'http://localhost:3000',
+        target: process.env.NODE_ENV === 'production' ? 'https://your-vercel-function-url.vercel.app' : 'http://localhost:3000',
         changeOrigin: true,
         pathRewrite: {
           '^/get_token': ''
