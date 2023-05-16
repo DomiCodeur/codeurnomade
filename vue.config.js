1db12502192f5b18daeb889ed4262387d22d5c29
matdomichard <mathieu.domichard@gmail.com>
2023-05-16 17:59:47 +0200

    Pour mep modif proxy

--- vue.config.js
+++ vue.config.js
@@ -1,8 +1,9 @@
+
 module.exports = {
   devServer: {
     proxy: {
       '/get_token': {
-        target: process.env.NODE_ENV === 'production' ? 'https://your-vercel-function-url.vercel.app' : 'http://localhost:3000',
+        target: process.env.NODE_ENV === 'production' ? '' : 'http://localhost:3000',
         changeOrigin: true,
         pathRewrite: {
           '^/get_token': ''
